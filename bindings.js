16691e8890b6e4e2994a4f72b8d1ecf33e2ae77a
Valentin <valpe70@gmail.com>
2018-05-22 15:44:05 -0400

    added brett

--- bindings.js
+++ bindings.js
@@ -1,5 +1,5 @@
 document.addEventListener('keydown', function(event) {
-  
+
     if(event.keyCode == 37) {
         console.log("HEYY");
         alert('Left was pressed');
@@ -7,22 +7,29 @@ document.addEventListener('keydown', function(event) {
     else if(event.keyCode == 49) {
         //alert('1 was pressed');
       console.log("YOUUUUUU");
-      
-      var audio = new Audio('cu_brett.wav');
+
+      var audio = new Audio('cu_brett2.m4a');
       audio.play();
     }
     else if(event.keyCode == 50) {
         //alert('1 was pressed');
       console.log("YOUUUUUU");
-      
+
       var audio = new Audio('cu_ishaan.wav');
       audio.play();
     }
     else if(event.keyCode == 51) {
         //alert('1 was pressed');
       console.log("YOUUUUUU");
-      
+
       var audio = new Audio('cu_valentin.wav');
+      audio.play();
+    }
+    else if(event.keyCode == 52) {
+        //alert('1 was pressed');
+      console.log("first brett one");
+
+      var audio = new Audio('cu_brett2.wav');
       audio.play();
     }
 });
